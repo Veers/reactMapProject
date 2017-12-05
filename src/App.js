42ab972df984df40c6642278bd17ef2e2768e9f2
Veers <a.konviser@gmail.com>
2017-12-06 01:13:44 +0300

    add bootstrap
    TROUBLE WITH COMMITS!! TEST CHANGES

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, {Component} from 'react';
 import Collapsible from './Collapsible';
 import ListData from './ListData';
-import EMap from './map/EMap';
-import logo from './logo.svg';
+import EMap from './map/MapWrapper';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+
                 <Collapsible title="List title" data={ListData}/>
                 <EMap/>
             </div>
@@ -24,3 +17,16 @@ class App extends Component {
 }
 
 export default App;
+
+
+/**
+ * import logo from './logo.svg';
+ *
+ * <header className="App-header">
+ <img src={logo} className="App-logo" alt="logo"/>
+ <h1 className="App-title">Welcome to React</h1>
+ </header>
+ <p className="App-intro">
+ To get started, edit <code>src/App.js</code> and save to reload.
+ </p>
+ */
--- src/Collapsible.js
+++ src/Collapsible.js
@@ -1,24 +1,40 @@
 import React, {Component} from 'react';
 import CollapsibleItem from './CollapsibleItem';
 import './Collapsible.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 class Collapsible extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {visibility: false};
+        this.toggleElement = this.toggleElement.bind(this);
+    }
+
+    toggleElement = () => this.changeToggle(!this.state.toggle);
+
+    changeToggle = (status) => this.setState({visibility: status});
 
     render() {
-        return (
-            <div className="collapsible">
-                <div className="list_header">
-                    {this.props.title}
+        if (this.state.visibility)
+            return (
+                <div className="collapsible show">
+                    <div className="list_header" onClick={this.toggleElement}>
+                        {this.props.title}
+                    </div>
+                    <ul>
+                        {this.props.data.map(function (item) {
+                            return <li key={item._id} className="list_item">
+                                <CollapsibleItem itemData={item}/>
+                            </li>;
+                        })}
+                    </ul>
                 </div>
-                <ul>
-                    {this.props.data.map(function (item) {
-                        return <li key={item._id} className="list_item">
-                            <CollapsibleItem itemData={item}/>
-                        </li>;
-                    })}
-                </ul>
-            </div>
-        );
+            );
+        else
+            return (
+                <div className="collapsible hidden" onClick={this.toggleElement}>
+                </div>
+            )
     }
 }
 
